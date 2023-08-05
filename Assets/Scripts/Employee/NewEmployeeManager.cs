7b942c821a18f01211533c72d7d61a34228c6385
LaM0uette <LaM0uette.dev@gmail.com>
2023-08-05 19:08:15 +0200

    add new employee

--- Assets/Scripts/Employee/NewEmployeeManager.cs
+++ Assets/Scripts/Employee/NewEmployeeManager.cs
@@ -4,12 +4,49 @@ namespace Employee
 {
     public class NewEmployeeManager : MonoBehaviour
     {
-        [SerializeField] private GameObject _buttonNewEmployee;
-        [SerializeField] private GameObject _employee;
+        #region Statements
+
+        private static GameManager _gameManager => GameManager.instance;
         
-        void Start()
+        [SerializeField] private static int _newEmployeePrice = 5000;
+        [SerializeField] private GameObject _buttonNewEmployee;
+        [SerializeField] private GameObject _employee; 
+
+        private void Start()
         {
             _employee.SetActive(false);
         }
+
+        #endregion
+
+        #region Functions
+
+        private void OnMouseDown()
+        {
+            CheckCostNewEmployee();
+        }
+
+        private void CheckCostNewEmployee()
+        {
+            if (_gameManager.Money < _newEmployeePrice) return;
+
+            BuyNewEmployee();
+        }
+        
+        private void BuyNewEmployee()
+        {
+            _gameManager.Money -= _newEmployeePrice;
+            _buttonNewEmployee.SetActive(false);
+            _employee.SetActive(true);
+
+            IncrementNewEmployeePrice();
+        }
+
+        private void IncrementNewEmployeePrice()
+        {
+            _newEmployeePrice += _newEmployeePrice;
+        }
+
+        #endregion
     }
 }
